31433d8f43f393fa29f1c051f1b7defb59159957
Kevin Tavera <kevin.tavera@gmail.com>
2014-06-11 18:02:08 -0400

    refactored to add different colors to column, row and the center number

--- myApp.js
+++ myApp.js
@@ -1,42 +1,45 @@
 angular.module('myApp', [])
-  .controller('DisplayCtrl', function($scope) {
-    $scope.$on('displayData', function(event, data) {
-      $scope.content = data;
+    .controller('DisplayCtrl', function ($scope) {
+        $scope.$on('displayData', function (event, data) {
+            $scope.content = data;
+        });
+    })
+    .controller('MultiplicationCtrl', function ($scope, $attrs, $rootScope) {
+        function populateNumbers(x) {
+            var numbers = [];
+            for (var i = 0; i < x; i++) {
+                numbers[i] = i + 1;
+            }
+            ;
+            return numbers;
+        }
+
+        $scope.compute = function (a, b) {
+            return a * b;
+        };
+
+        $scope.$watch("numberLimit", function (limit) {
+            $scope.numbers = populateNumbers(limit);
+        });
+
+        $scope.numberLimit = $attrs.initialNumberLimit || 10;
+
+        $scope.activeFactorA = 0;
+        $scope.activeFactorB = 0;
+        $scope.setActiveFactors = function (a, b) {
+            $scope.activeFactorA = a;
+            $scope.activeFactorB = b;
+        };
+
+        $scope.matchesFactor = function (a, b) {
+            return a === $scope.activeFactorA || b === $scope.activeFactorB;
+        };
+
+        $scope.setActiveNumber = function (number) {
+            $rootScope.$broadcast('displayData', number);
+        };
+
+        $scope.clearActiveFactors = function () {
+            activeFactorA = activeFactorB = null;
+        };
     });
-  })
-  .controller('MultiplicationCtrl', function($scope, $attrs, $rootScope) {
-    function populateNumbers(x) {
-      var numbers = [];
-      for(var i=0; i<x; i++) 
-      {numbers[i] = i + 1; };
-      return numbers;
-    }
-
-    $scope.compute = function(a,b) {
-       return a * b;
-    };
-
-    $scope.$watch("numberLimit", function(limit) {
-      $scope.numbers = populateNumbers(limit);
-    });
-
-    $scope.numberLimit = $attrs.initialNumberLimit || 10;
-    
-    var activeFactorA, activeFactorB;
-    $scope.setActiveFactors = function(a, b) {
-      activeFactorA = a;
-      activeFactorB = b;
-    };
-
-    $scope.matchesFactor = function(a, b) {
-      return a === activeFactorA || b === activeFactorB;
-    };
-    
-    $scope.setActiveNumber = function(number) {
-      $rootScope.$broadcast('displayData', number);
-    };
-      
-    $scope.clearActiveFactors = function() {
-      activeFactorA = activeFactorB = null;
-    };
-  });
